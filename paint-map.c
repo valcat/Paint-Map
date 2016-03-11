9c34c9c9f0f58bce2a5bdac085fee7560a9e1824
valcat <val.gorodok@gmail.com>
2016-03-11 14:39:22 +0300

    Add constants, method draw_buttons, and new structs

--- paint-map.c
+++ paint-map.c
@@ -5,28 +5,34 @@
 #include <GL/glut.h>
 #include <stdbool.h>
 
-int height_window = 800;
-int width_window = 800;
+const int HEIGHT_WINDOW = 800;
+const int WIDTH_WINDOW = 800;
+const int PANEL_BORD_PADDING = 60;
+const char* WINDOW_NAME = "CAT-MAP";
 int flag = 0;
+
+typedef struct Point
+{
+  int x;
+  int y;
+} Point;
+
+typedef struct Rectangle
+{
+  Point point;
+  int width;
+  int height;
+} Rectangle;
 
 typedef struct Panel_border
 {
-  //coordinates of panel's border
-  int x_border;
-  int y_border;
-  int width_border;
-  int height_border;
-  
+  Rectangle rect;
 } Panel_border;
 
 typedef struct Button
 {
-  //tools for drawing
-  //rectangle
-  int x_rect;
-  int y_rect;
-  int width_rect;
-  int height_rect; 
+  Rectangle rect;
+
   //triangle
   int x_tr_1st;
   int y_tr_1st;
@@ -39,67 +45,66 @@ typedef struct Button
   int y_line_1st;
   int x_line_2d;
   int y_line_2d;
-  
 } Button;
 
 
 Panel_border* border = NULL;
-Button* tool_rect = NULL;
-Button* tool_triangle = NULL;
-Button* tool_line = NULL;
+Button* button_rect = NULL;
+Button* button_triangle = NULL;
+Button* button_line = NULL;
 
 Panel_border* create_panel_border(int x, int y, int width, int height)
 {
   Panel_border* border = malloc(sizeof(Panel_border));
-  border->x_border = x;
-  border->y_border = y;
-  border->width_border = width;
-  border->height_border = height;
+  border->rect.point.x = x;
+  border->rect.point.y = y;
+  border->rect.width= width;
+  border->rect.height= height;
 
   return border;
 }
 
-Button* create_tool_rect(int x, int y, int width, int height)
+Button* create_button_rect(int x, int y, int width, int height)
 {
-  Button* tool_rect = malloc(sizeof(Button));
-  tool_rect->x_rect = x;
-  tool_rect->y_rect = y;
-  tool_rect->width_rect = width;
-  tool_rect->height_rect = height;
+  Button* button_rect = malloc(sizeof(Button));
+  button_rect->rect.point.x = x;
+  button_rect->rect.point.y = y;
+  button_rect->rect.width = width;
+  button_rect->rect.height = height;
 
-  return tool_rect;
+  return button_rect;
 }
 
-Button* create_tool_triangle(int x1, int y1, int x2, int y2, int x3,  int y3)
+Button* create_button_triangle(int x1, int y1, int x2, int y2, int x3,  int y3)
 {
-  Button* tool_triangle = malloc(sizeof(Button));
-  tool_triangle->x_tr_1st = x1;
-  tool_triangle->y_tr_1st = y1;
-  tool_triangle->x_tr_2d = x2;
-  tool_triangle->y_tr_2d = y2;
-  tool_triangle->x_tr_3d = x3;
-  tool_triangle->y_tr_3d = y3;
+  Button* button_triangle = malloc(sizeof(Button));
+  button_triangle->x_tr_1st = x1;
+  button_triangle->y_tr_1st = y1;
+  button_triangle->x_tr_2d = x2;
+  button_triangle->y_tr_2d = y2;
+  button_triangle->x_tr_3d = x3;
+  button_triangle->y_tr_3d = y3;
 
-  return tool_triangle;
+  return button_triangle;
 }
 
-Button* create_tool_line(int x1, int y1, int x2, int y2)
+Button* create_button_line(int x1, int y1, int x2, int y2)
 {
-  Button* tool_line = malloc(sizeof(Button));
-  tool_line->x_line_1st = x1;
-  tool_line->y_line_1st = y1;
-  tool_line->x_line_2d = x2;
-  tool_line->y_line_2d = y2;
+  Button* button_line = malloc(sizeof(Button));
+  button_line->x_line_1st = x1;
+  button_line->y_line_1st = y1;
+  button_line->x_line_2d = x2;
+  button_line->y_line_2d = y2;
 
-  return tool_line;
+  return button_line;
 }
 
 void init(void)
 {
   border = create_panel_border(0, 0, 60, 800);
-  tool_rect = create_tool_rect(20, 740, 20, 20);
-  tool_triangle = create_tool_triangle(30, 680, 20, 660, 40, 660);
-  tool_line = create_tool_line(20, 600, 40, 620);
+  button_rect = create_button_rect(20, 740, 20, 20);
+  button_triangle = create_button_triangle(30, 680, 20, 660, 40, 660);
+  button_line = create_button_line(20, 600, 40, 620);
 }
 
 void reshape(int width, int height)
@@ -115,10 +120,10 @@ void reshape(int width, int height)
 void draw_panel(Panel_border* border)
 {
   int x, y, width, height;
-  x = border->x_border;
-  y = border->y_border;
-  width = border->width_border;
-  height = border->height_border;
+  x = border->rect.point.x;
+  y = border->rect.point.y;
+  width = border->rect.width;
+  height = border->rect.height;
 
   glColor3f(0.9, 0.9, 0.9);
   glBegin(GL_QUADS);              
@@ -141,7 +146,7 @@ void draw_greed(void)
 
   int i = 0;
   int step = 20;
-  int num_lines = width_window / step;
+  int num_lines = WIDTH_WINDOW / step;
 
   //vertical lines
   for (i = 0; i < num_lines; i++) {
@@ -149,7 +154,7 @@ void draw_greed(void)
     glLineWidth(1);
     glBegin(GL_LINES);
       glVertex2d(x_vert, y_vert);
-      glVertex2d(x_vert, height_window);
+      glVertex2d(x_vert, HEIGHT_WINDOW);
     glEnd();
     x_vert = x_vert + step;
     glFlush();
@@ -160,20 +165,20 @@ void draw_greed(void)
     glLineWidth(1);
     glBegin(GL_LINES);
       glVertex2d(x_hor, y_hor);
-      glVertex2d(width_window, y_hor);
+      glVertex2d(WIDTH_WINDOW, y_hor);
     glEnd();
     y_hor = y_hor + step;
     glFlush();
   }
 }
 
-void draw_rectangle(Button *tool_rect) 
+void draw_rectangle_button(Button *button_rect) 
 {
   int x, y, width, height;
-  x = tool_rect->x_rect;
-  y = tool_rect->y_rect;
-  width = tool_rect->width_rect;
-  height = tool_rect->height_rect;
+  x = button_rect->rect.point.x;
+  y = button_rect->rect.point.y;
+  width = button_rect->rect.width;
+  height = button_rect->rect.height;
 
   glColor3f(0.0, 1.0, 0.0);
   glBegin(GL_QUADS);              
@@ -186,15 +191,15 @@ void draw_rectangle(Button *tool_rect)
   glutSwapBuffers();
 }
 
-void draw_triangle(Button* tool_triangle)
+void draw_triangle_button(Button* button_triangle)
 {
   int x1, x2, x3, y1, y2, y3;
-  x1 = tool_triangle->x_tr_1st;
-  y1 = tool_triangle->y_tr_1st;
-  x2 = tool_triangle->x_tr_2d;
-  y2 = tool_triangle->y_tr_2d;
-  x3 = tool_triangle->x_tr_3d;
-  y3 = tool_triangle->y_tr_3d;
+  x1 = button_triangle->x_tr_1st;
+  y1 = button_triangle->y_tr_1st;
+  x2 = button_triangle->x_tr_2d;
+  y2 = button_triangle->y_tr_2d;
+  x3 = button_triangle->x_tr_3d;
+  y3 = button_triangle->y_tr_3d;
 
   glColor3f(1.0, 0.0, 0.3);
   glBegin(GL_TRIANGLES);               
@@ -206,13 +211,13 @@ void draw_triangle(Button* tool_triangle)
   glutSwapBuffers();
 }
 
-void draw_line(Button* tool_line) 
+void draw_line_button(Button* button_line) 
 {
   int x1, y1, x2, y2;
-  x1 = tool_line->x_line_1st;
-  y1 = tool_line->y_line_1st;
-  x2 = tool_line->x_line_2d;
-  y2 = tool_line->y_line_2d;
+  x1 = button_line->x_line_1st;
+  y1 = button_line->y_line_1st;
+  x2 = button_line->x_line_2d;
+  y2 = button_line->y_line_2d;
  
   glColor3f(0.0,0.4,0.2); 
   glPointSize(3.0);  
@@ -225,6 +230,13 @@ void draw_line(Button* tool_line)
   glutSwapBuffers();
 } 
 
+void draw_buttons(void)
+{
+  draw_rectangle_button(button_rect);
+  draw_triangle_button(button_triangle);
+  draw_line_button(button_line);
+}
+
 void draw(void)
 {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -233,9 +245,7 @@ void draw(void)
   //draw working window and panel with set of tools
   draw_greed();
   draw_panel(border);
-  draw_rectangle(tool_rect);
-  draw_triangle(tool_triangle);
-  draw_line(tool_line);
+  draw_buttons();
 }
 
 //to delete
@@ -271,8 +281,7 @@ void draw_line2(int x1, int y1)
 
 void mouse(int button, int state, int x, int y)
 {
-  int new_y = height_window - y;
-  int panel_bord_end = 60;
+  int new_y = HEIGHT_WINDOW - y;
   printf("x - %d\n", x);
   printf("y - %d\n", new_y);
 
@@ -281,10 +290,9 @@ void mouse(int button, int state, int x, int y)
   }
   switch (flag) {
     case 1:
-      if (x > panel_bord_end) {
-        //draw_rectangle2(x, new_y);
-        draw_line2(x, new_y);
-        printf("FO\n");
+      if (x > PANEL_BORD_PADDING) {
+        draw_rectangle2(x, new_y);
+        printf("Yes\n");
       }
       break;
     case 2:
@@ -298,16 +306,18 @@ int main(int argc, char *argv[])
   init();
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_RGBA | GLUT_ALPHA | GLUT_DEPTH);
-  glutInitWindowSize(height_window, width_window);
+  glutInitWindowSize(HEIGHT_WINDOW, WIDTH_WINDOW);
   glutInitWindowPosition(0, 0);
-  glutCreateWindow("Cat-pensil");
+  glutCreateWindow(WINDOW_NAME);
   glutReshapeFunc(reshape);
   glutDisplayFunc(draw);
   glClearColor(1.0, 1.0, 1.0, 1.0);
   glutMouseFunc(mouse);
   glutMainLoop();
   free(border);
-  free(tool_rect);
+  free(button_rect);
+  free(button_triangle);
+  free(button_line);
 
   return 0;
 }
