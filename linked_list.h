e872e91911bac789f869fb03b31d96c3dcddf370
valcat <val.gorodok@gmail.com>
2016-07-19 17:24:07 +0300

    Header file.

---
+++ linked_list.h
@@ -0,0 +1,19 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+typedef struct Node_coordinates
+{
+  int x, y;
+  struct Node_coordinates *next;
+} Node_coordinates;
+
+typedef struct Linked_list
+{
+  Node_coordinates *head;
+} Linked_list;
+
+Linked_list* createLinkedList();
+void addNode(Linked_list *linked_list, int x, int y);
+void freeNodes(Linked_list *linked_list);
+
+#endif
