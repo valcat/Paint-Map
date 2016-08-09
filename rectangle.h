3a824585d5411f6f0adb174c691e1b5459a0e50d
valcat <val.gorodok@gmail.com>
2016-08-09 14:27:34 +0300

    Header file of rectangle.c.

---
+++ rectangle.h
@@ -0,0 +1,15 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+#include "allstructs.h"
+
+/* rectangle */
+extern const int X_RECT;
+extern const int Y_RECT;
+extern const int WIDTH_RECT;
+extern const int HEIGHT_RECT;
+
+
+Button* initRect();
+void drawRectangleButton(Button* button_rect);
+
+#endif
