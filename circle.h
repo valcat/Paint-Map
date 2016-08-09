b2333975dc42697e72aafad2a52105c1f8fc7058
valcat <val.gorodok@gmail.com>
2016-08-09 14:30:52 +0300

    Moved structs into separate file.

--- circle.h
+++ circle.h
@@ -1,5 +1,6 @@
 #ifndef CIRCLE_H
 #define CIRCLE_H
+#include "allstructs.h"
 
 extern const int X_CRL;
 extern const int Y_CRL;
@@ -7,21 +8,9 @@ extern const int RADS;
 extern const int NUM_SGMTS;
 extern const int SEGMENT;
 
-typedef struct Point_c
-{
-  int x;
-  int y;
-} Point_c;
 
-typedef struct Button_c
-{
-  Point_c point_circle;
-  int radius;
-  int num_segments;
-} Button_c;
-
-Button_c* createButtonCircle(int x, int y, int radius, int num_segments);
-Button_c* initCircle();
-void drawCircleButton(Button_c* button_circle);
+Button* createButtonCircle(int x, int y, int radius, int num_segments);
+Button* initCircle();
+void drawCircleButton(Button* button_circle);
 
 #endif
