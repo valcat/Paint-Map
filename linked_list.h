940d9187f417c47b153b28002d6c676efd653030
valcat <val.gorodok@gmail.com>
2016-08-11 16:08:23 +0300

    Removed function freeNode.

--- linked_list.h
+++ linked_list.h
@@ -16,6 +16,5 @@ Linked_list* createLinkedList();
 void addNode(Linked_list* linked_list, void* x);
 size_t count(Linked_list *linked_list);
 Node* getByIndex(Linked_list *linked_list, int index);
-void freeNodes(Linked_list *linked_list);
 
 #endif
