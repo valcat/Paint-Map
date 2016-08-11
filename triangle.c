f9d8307717fc49d29f4b74219b478b7f8e234ca8
valcat <val.gorodok@gmail.com>
2016-08-11 16:12:13 +0300

    Modified the code in accordance with changes in the main file (creating struct MapState with exemplars of other structs.).

--- triangle.c
+++ triangle.c
@@ -19,24 +19,25 @@ const int Y_SCD_T = 570;
 const int Y_TRD_T = 570;
 const int STEP_T = 20;
 
+MapState stateTriangle;
+
 Button* createButtonTriangle(int x1, int y1, int x2, int y2, int x3,  int y3)
 {
-  Button* button_triangle = malloc(sizeof(Button));
-  button_triangle->first_point.x = x1;
-  button_triangle->first_point.y = y1;
-  button_triangle->sec_point.x = x2;
-  button_triangle->sec_point.y = y2;
-  button_triangle->third_point.x = x3;
-  button_triangle->third_point.y = y3;
+  stateTriangle.button_triangle = malloc(sizeof(Button));
+  stateTriangle.button_triangle->first_point.x = x1;
+  stateTriangle.button_triangle->first_point.y = y1;
+  stateTriangle.button_triangle->sec_point.x = x2;
+  stateTriangle.button_triangle->sec_point.y = y2;
+  stateTriangle.button_triangle->third_point.x = x3;
+  stateTriangle.button_triangle->third_point.y = y3;
 
-  return button_triangle;
+  return stateTriangle.button_triangle;
 }
 
 Button* initTriangle()
 {
-	Button* button_triangle = NULL;
-	button_triangle = createButtonTriangle(X_FST_T, Y_FST_T, X_SND_T, Y_SCD_T, X_TRD_T, Y_TRD_T);
-	return button_triangle;
+	stateTriangle.button_triangle = createButtonTriangle(X_FST_T, Y_FST_T, X_SND_T, Y_SCD_T, X_TRD_T, Y_TRD_T);
+	return stateTriangle.button_triangle;
 }
 
 void drawTriangleButton(Button* button_triangle)
