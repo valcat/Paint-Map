8b93d9b18097b30e020ca51617bc3f663e2a0439
valcat <val.gorodok@gmail.com>
2016-08-12 23:07:28 +0300

    Made function getByIndex void*.

--- linked_list.h
+++ linked_list.h
@@ -15,6 +15,6 @@ typedef struct Linked_list
 Linked_list* createLinkedList();
 void addNode(Linked_list* linked_list, void* x);
 size_t count(Linked_list *linked_list);
-Node* getByIndex(Linked_list *linked_list, int index);
+void* getByIndex(Linked_list *linked_list, int index);
 
 #endif
