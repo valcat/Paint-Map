41e1b5ace0d830f67b24e7dc29f7d3e3d82aec05
valcat <val.gorodok@gmail.com>
2016-10-25 23:57:59 +0300

    Header file of graph.c.

---
+++ graph.h
@@ -0,0 +1,9 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#include "allstructs.h"
+#include "linked_list.h"
+
+Edge* createEdge(Point* point_1st, Point* point_2nd);
+void createEdges(Point* point1, Point* point2);
+
+#endif
