5c6f2acee24eeb1864d9c1398c97912414a92e3f
valcat <val.gorodok@gmail.com>
2016-11-08 23:52:04 +0300

    Header file of find_path.c.

---
+++ find_path.h
@@ -0,0 +1,8 @@
+#ifndef FIND_PATH_H
+#define FIND_PATH_H
+#include "allstructs.h"
+#include "linked_list.h"
+
+double findLengthOfEdge(int point1_x, int point1_y, int point2_x, int point2_y) 
+
+#endif
