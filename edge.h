95a0e7d0ad04c09240daa06d29fa7baddb1dab6b
valcat <val.gorodok@gmail.com>
2016-11-16 01:12:17 +0300

    Added function drawEdges().

---
+++ edge.h
@@ -0,0 +1,9 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#include "allstructs.h"
+#include "linked_list.h"
+
+Edge* createEdge(Point* point_1st, Point* point_2nd);
+void drawEdges(Linked_list* linked_list);
+
+#endif
