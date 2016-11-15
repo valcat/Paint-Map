5a8524bde6af0768684271fd0932ecd68ccf69b5
valcat <val.gorodok@gmail.com>
2016-11-16 02:07:20 +0300

    Added function drawEdgeVer

--- edge.h
+++ edge.h
@@ -1,9 +1,12 @@
-#ifndef GRAPH_H
-#define GRAPH_H
+#ifndef EDGE_H
+#define EDGE_H
 #include "allstructs.h"
 #include "linked_list.h"
+#include "circle.h"
 
 Edge* createEdge(Point* point_1st, Point* point_2nd);
+void saveEdge(Linked_list* linked_list, Point* point1, Point* point2);
 void drawEdges(Linked_list* linked_list);
+void drawEdgeVertices(Linked_list* linked_list, int radius);
 
 #endif
