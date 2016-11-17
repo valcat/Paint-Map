6bc8106053f6b0b0d319871bdbbc1124eadd05ea
valcat <val.gorodok@gmail.com>
2016-11-17 23:58:22 +0300

    Added some function from paint-map.c.

--- edge.h
+++ edge.h
@@ -4,9 +4,12 @@
 #include "linked_list.h"
 #include "circle.h"
 
-Edge* createEdge(Point* point_1st, Point* point_2nd);
-void saveEdge(Linked_list* linked_list, Point* point1, Point* point2);
+Edge* saveEdge(Point* point_1st, Point* point_2nd);
+//void saveEdge(Linked_list* linked_list, Point* point1, Point* point2);
 void drawEdges(Linked_list* linked_list);
 void drawEdgeVertices(Linked_list* linked_list, int radius);
+double findLengthOfEdge(int point1_x, int point1_y, int point2_x, int point2_y);
+void printLengthOfEdge(Linked_list* linked_list);
+
 
 #endif
